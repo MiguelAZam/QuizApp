14d30b2cf5178f78b3b2310b52275cd0c981e4c2
Miguel <miguelzamudio.dev@hotmail.com>
2020-02-17 08:20:06 -0700

    Add pubspec.yaml for http package and QuizAPI functionality

---
+++ QuizAPI.dart
@@ -0,0 +1,20 @@
+import 'dart:collection';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+
+class QuizAPI {
+  final String BASE_URL = "http://www.cs.utep.edu/cheon/cs4381/homework/quiz";
+
+  Future<HashMap<String, dynamic>> getQuiz(String quizName) async {
+    var url = '$BASE_URL?quiz=$quizName';
+    var resp = await http.get(url);
+    var jsonResponse = convert.jsonDecode(resp.body);
+    if (jsonResponse["response"]) {
+      return jsonResponse["quiz"];
+    } else {
+      var errorReason = jsonResponse["reason"];
+      print('Error: $errorReason');
+      return null;
+    }
+  }
+}
